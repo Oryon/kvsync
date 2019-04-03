141d2c78d619dd4222607dd807a706e0292a2866
Pierre Pfister <ppfister@cisco.com>
2019-04-03 10:46:34 +0200

    Add example to test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
-cd $GOPATH
+CD="$( cd "$( dirname $0 )" && pwd )"
+cd $CD/../
+
+# Uncomment to use gopath instead of gomod
+# cd $GOPATH
+
+echo "===== Unit testing ====="
 go test -v -gcflags "-N -l" github.com/Oryon/kvsync/encoding github.com/Oryon/kvsync/sync github.com/Oryon/kvsync/store
+
+echo "===== Examples ====="
+echo "Running examples/sync1"
+go run github.com/Oryon/kvsync/examples/sync1 > /dev/null
